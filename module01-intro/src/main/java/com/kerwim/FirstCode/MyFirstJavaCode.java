9511e6068e34b3d5ec08a766cfd28146ca56c741
mac <0924>
2024-10-14 21:00:47 +0800

    概述和基础语法

---
+++ module01-intro/src/main/java/com/kerwim/FirstCode/MyFirstJavaCode.java
@@ -0,0 +1,12 @@
+package com.kerwim.FirstCode;
+
+
+public class MyFirstJavaCode {
+
+    public class HelloWorld {
+        public static void main(String[] args) {
+            System.out.println("HelloWorld");
+        }
+    }
+
+}
