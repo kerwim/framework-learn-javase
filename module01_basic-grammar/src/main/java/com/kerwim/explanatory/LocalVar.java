0cb40e8c1d375c07b394ca786dd76bfdb78cf907
mac <0924>
2025-01-17 11:34:04 +0800

    first commit

---
+++ module01_basic-grammar/src/main/java/com/kerwim/explanatory/LocalVar.java
@@ -0,0 +1,14 @@
+package com.kerwim.explanatory;
+
+import java.math.BigDecimal;
+
+public class LocalVar {
+    private int a;
+    static int b;
+
+    public static void main(String[] args) {
+        LocalVar lv = new LocalVar();
+        System.out.println(lv.a);
+        System.out.println(b);
+    }
+}
