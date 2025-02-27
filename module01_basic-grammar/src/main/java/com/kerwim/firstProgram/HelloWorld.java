cb2e9c87f31890c7f7b0e2c2fd530a1e43417fc8
mac <0924>
2025-02-27 21:32:43 +0800

    first commit

---
+++ module01_basic-grammar/src/main/java/com/kerwim/firstProgram/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.kerwim.firstProgram;
+
+/**
+ * HelloWorld 第一个 Java 程序
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello,world!");
+    }
+}
